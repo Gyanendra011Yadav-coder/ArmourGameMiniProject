af0a6a46fbb6350c39b3f6014668934f50a7339a
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-05 12:15:54 -0700

    Created Some variables, and others are extended from the player1.java class

--- src/Player2.java
+++ src/Player2.java
@@ -1,3 +1,11 @@
 public class Player2 extends Player1{
+    private int health;
+    private boolean armour;
 
+    public Player2(String name, String Weapon , int health , boolean armour ){
+        super(name,Weapon,health);  // As, we are extending it from Player1, so we are storing the value of the player 1 to player 2. It is done with the help of super keyword.
+        this.health=health;
+        this.armour=armour;
+
+    }
 }
