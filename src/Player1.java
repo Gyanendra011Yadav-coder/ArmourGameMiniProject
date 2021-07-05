e7ad396c678add99012f4d52519a7131db99fc06
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-05 11:26:36 -0700

    Created,Method that will store,The Health of the Player1, when that is hitted by Gun-2.

--- src/Player1.java
+++ src/Player1.java
@@ -14,11 +14,23 @@ public class Player1 {
         if (this.health<=0){
             this.health=0;
         }
-        System.out.println("!! HITTED BY GUN-1" +
+        System.out.println("!! HITTED  BY GUN-1" +
                 "HEALTH -30\n"+
         "#-HEALTH LEFT-#-"+this.health);
+
+        if (this.health==0){
+            System.out.println("PLAYER DIED.");
+        }
     }
     public void damageByGun2(){
-
+        this.health=50;
+        if (this.health<=0){
+            System.out.println("!! HITTED  BY GUN-2" +
+                    "HEALTH -50\n"+
+                    "#-HEALTH LEFT-#-"+this.health);
+            if (this.health==0){
+                System.out.println("Player Died");
+            }
+        }
     }
 }
