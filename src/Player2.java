1b4b3d0bfc2167d7e51115ca1067205f78b8059d
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-05 12:31:30 -0700

    WROTE SOME COMMENTS &
    AND JUST OVERRIDE THE damageByGun1() method. and there super keyword is showing this is not the member of the present class, it is overRidden.

--- src/Player2.java
+++ src/Player2.java
@@ -3,9 +3,14 @@ public class Player2 extends Player1{
     private boolean armour;
 
     public Player2(String name, String Weapon , int health , boolean armour ){
+        /*Super():-THIS METHOD SHOWS THAT, THE VARIABLES ARE NOT OF THE PRESENT CLASS, THEY ARE EXTENDED FROMS SOME ANOTHER CLASS.*/
         super(name,Weapon,health);  // As, we are extending it from Player1, so we are storing the value of the player 1 to player 2. It is done with the help of super keyword.
         this.health=health;
         this.armour=armour;
+    }
 
+    @Override
+    public void damageByGun1() {
+        super.damageByGun1();
     }
 }
