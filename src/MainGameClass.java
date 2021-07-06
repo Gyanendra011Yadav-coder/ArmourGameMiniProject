15423d609d97dbb18b3dc18189ed26a87151cc8e
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-06 14:13:57 -0700

    wROTE STATEMENT TO CHOOSE THE OPTIONS FOR THE PLAYER 1.

--- src/MainGameClass.java
+++ src/MainGameClass.java
@@ -5,13 +5,33 @@ public class MainGameClass {
         Scanner sc = new Scanner(System.in);
         System.out.println("||==>>Welcome To Armour Game<<==||");
         System.out.println("ENTER THE NAME OF PLAYER 1st:");
-        String playerName1=sc.nextLine();
+        String playerName1 = sc.nextLine();
         System.out.println("ENTER THE NAME OF PLAYER 2nd:");
-        String playerName2=sc.nextLine();
-        System.out.println("Hello,==>>"+playerName1+" "+",&,"+playerName2+"\n" +
+        String playerName2 = sc.nextLine();
+        System.out.println("Hello,==>>" + playerName1 + " " + ",&," + playerName2 + "\n" +
                 "Armour Game Welcomes You, Whole Heartedly\n" +
                 "So,Let's Rolling In To Game=====!!!!!!");
-
+        System.out.println("It's Player 1st Chance To Select:-");
+        System.out.println("DO YOU WANT TO USE GUN TYPE-01 oR GUN TYPE-02: **type 1 or 2**");
+        int gunType = sc.nextInt();
+        if (gunType == 1){
+            System.out.println("CHOOSE GUN FROM THE LIST OF THE TYPE-01:\n" +
+                    "1.ump-19\n" +
+                    "2.Scar-L\n" +
+                    "3.M416\n" +
+                    "4.Pistol");
+        int gun = sc.nextInt();
+    }else{
+            System.out.println("CHOOSE GUN FROM THE LIST TYPE-02:\n" +
+                    "1.AWM\n" +
+                    "2.SNIPPER\n" +
+                    "3.GORILLA\n" +
+                    "4.GIANT SNIPPER\n");
+            int gun = sc.nextInt();
+        }
+        System.out.println();
+        System.out.println();
+        System.out.println("CHOOSE EQUIPMENTS FOR PLAYER-2:");
 
 
         Player1 player = new Player1("Gyan","AWM",100);
