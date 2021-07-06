6ea6674f160a9165c9d1f14e39a3af6a9c36c44d
Gyanendra011Yadav-coder <gyanendra.yadav_cs19@gla.ac.in>
2021-07-06 14:28:26 -0700

    WROTE THE STATEMENT FOR RECOGNIZING THE VALUE FOR PLAYER 1 && PASSING TO THA player1.java class.
    and Also, wrote the statement for player 2.,

--- src/MainGameClass.java
+++ src/MainGameClass.java
@@ -14,34 +14,79 @@ public class MainGameClass {
         System.out.println("It's Player 1st Chance To Select:-");
         System.out.println("DO YOU WANT TO USE GUN TYPE-01 oR GUN TYPE-02: **type 1 or 2**");
         int gunType = sc.nextInt();
+        int gun1=0;
         if (gunType == 1){
             System.out.println("CHOOSE GUN FROM THE LIST OF THE TYPE-01:\n" +
                     "1.ump-19\n" +
                     "2.Scar-L\n" +
                     "3.M416\n" +
                     "4.Pistol");
-        int gun = sc.nextInt();
+        gun1 = sc.nextInt();
     }else{
             System.out.println("CHOOSE GUN FROM THE LIST TYPE-02:\n" +
                     "1.AWM\n" +
                     "2.SNIPPER\n" +
                     "3.GORILLA\n" +
                     "4.GIANT SNIPPER\n");
-            int gun = sc.nextInt();
+            gun1 = sc.nextInt();
         }
+        String gunName = null;
+        if (gunType==1 && gun1==1){
+            gunName="ump-19";
+        }else if (gunType==1 && gun1==2){
+            gunName="Scar-L";
+        }else if (gunType==1 && gun1==3){
+            gunName="M416";
+        }else if (gunType==1 && gun1==4){
+            gunName="Pistol";
+        }else if (gunType==2 && gun1==1){
+            gunName="AWM";
+        }else if (gunType==2 && gun1==2){
+            gunName="SNIPPER";
+        }else if (gunType==2 && gun1==3){
+            gunName="GORILLA";
+        }else if (gunType==2 && gun1==4){
+            gunName="GIANT SNIPPER";
+        }else {
+            System.out.println("TYPE MISMATCH");
+
+        }
+        Player1 player = new Player1(playerName1,gunName,100);
+        System.out.println("HEALTH OF PLAYER-1 IS:-"+player.getHealth());
+
         System.out.println();
         System.out.println();
         System.out.println("CHOOSE EQUIPMENTS FOR PLAYER-2:");
+        System.out.println("DO YOU WANT TO USE GUN TYPE-01 oR GUN TYPE-02: **type 1 or 2**");
+        int gunType2 = sc.nextInt();
+        if (gunType2 == 1){
+            System.out.println("CHOOSE GUN FROM THE LIST OF THE TYPE-01:\n" +
+                    "1.ump-19\n" +
+                    "2.Scar-L\n" +
+                    "3.M416\n" +
+                    "4.Pistol");
+            int gun2 = sc.nextInt();
+        }else{
+            System.out.println("CHOOSE GUN FROM THE LIST TYPE-02:\n" +
+                    "1.AWM\n" +
+                    "2.SNIPPER\n" +
+                    "3.GORILLA\n" +
+                    "4.GIANT SNIPPER\n");
+            int gun2 = sc.nextInt();
+        }
+        System.out.println("PLAYER-2,HAS ONE EXTRA FEATURE THAN PLAYER-01, OF ARMOUR:-\n" +
+                "DO YOU WANT TO ON IT:-**true or false**");
+        boolean armourOnOff=sc.nextBoolean();
 
 
-        Player1 player = new Player1("Gyan","AWM",100);
+
 //        System.out.println(player.getName());
 //        System.out.println(player.getWeapons());
 //        System.out.println(player.getHealth());
-        player.damageByGun1();
-        player.damageByGun2();
+//        player.damageByGun1();
 //        player.damageByGun2();
-        player.heal();
+//        player.damageByGun2();
+//        player.heal();
 //
 //        Player2 player2 = new Player2("John","M416",50,false);
 //        player2.damageByGun1();
